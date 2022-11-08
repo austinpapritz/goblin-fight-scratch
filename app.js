0b4989bf35c478b03eddff2d70bcabaf802cff37
Austin Papritz <austin@papritz.dev>
2022-11-08 10:17:53 -0800

    form eventlistener adds baby object with name, currentId, and random hunger 1-3; currentId increments

--- app.js
+++ app.js
@@ -1,16 +1,35 @@
 /* Imports */
 import { renderBaby } from './render.js';
+
 /* Get DOM Elements */
+const formEl = document.querySelector('form');
 
 /* State */
+let currentId = 3;
 
-// let babies = [
-//     { name: Joey, hunger: 3 },
+let babies = [
+    { id: 1, name: 'Joey', hunger: 3 },
 
-//     { name: Abby, hunger: 3 },
-// ];
+    { id: 2, name: 'Abby', hunger: 3 },
+];
 
 /* Events */
+formEl.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const data = new FormData(formEl);
+
+    const newBaby = {
+        id: currentId,
+        name: data.get('baby-name'),
+        hunger: Math.ceil(Math.random() * 3),
+    };
+
+    currentId++;
+
+    babies.push(newBaby);
+    console.log(babies);
+});
 
 /* Display Functions */
 
